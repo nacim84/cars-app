4f8c63686288bfa3d608b3cdc0f4c6bc7efd9c50
Nacim Rabia <rabia.nacim@yahoo.com>
2022-09-04 22:53:08 +0200

    Quuick fix Router Dom Issue

--- react-ts-app/src/CarsApp.tsx
+++ react-ts-app/src/CarsApp.tsx
@@ -1,19 +1,23 @@
 import { FC } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/home/Home";
 import { Layout } from "./shared/layouts/Layout";
 
 export const CarsApp: FC = () => {
   return (
-    <div>
-      {/* <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<Home />} />
-        <Route path="/sign-in" element={<Home />} />
-        <Route path="/sign-up" element={<Home />} />
-        <Route path="/users/create" element={<Home />} />
-        <Route path="/cars/create" element={<Home />} />
-        <Route path="/users/:id" element={<Home />} />
-      </Routes> */}
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Home />} />
+          <Route path="/sign-in" element={<Home />} />
+          <Route path="/sign-up" element={<Home />} />
+          <Route path="/users/create" element={<Home />} />
+          <Route path="/cars/create" element={<Home />} />
+          <Route path="/users/:id" element={<Home />} />
+        </Routes>
+      </Router>
       <Layout />
-    </div>
+    </>
   );
 };
